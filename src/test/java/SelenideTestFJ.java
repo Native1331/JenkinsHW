b135b05b331098c62492e3910a8416ba7c6a7b0f
Natalya.Kadysheva <Native1331>
2022-05-01 13:44:56 +0300

    Initial commit

---
+++ src/test/java/SelenideTestFJ.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTestFJ {
+
+    @Test
+    void shouldFindSelenide() throws InterruptedException {
+        //открыть страницу github.com
+        open("https://github.com");
+        //ввести в поле поиска selenide и нажать Enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        sleep(5000);
+        //нажать на линк от первого результата поиска
+        $$("ul.repo-list li").first().$("a").click();
+        //проверка:
+        $("h2").shouldHave(Condition.text("selenide / selenide"));
+    }
+}
+
+
